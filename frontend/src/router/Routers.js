24ecf23b31fc9bfca300e40971b22203770fe48f
Vinan <80715419+AnnaTrojanowska@users.noreply.github.com>
2023-05-13 20:31:22 +0200

    add new files

---
+++ frontend/src/router/Routers.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Route, Routes, Navigate} from 'react-router-dom'
+import Home from './../pages/Home'
+import Login from './../pages/Login'
+import Register from './../pages/Register'
+import SearchResultList from './../pages/SearchResultList'
+import TourDetails from './../pages/TourDetails'
+import Tours from './../pages/Tours'
+
+const Routers = () => {
+  return (
+    <Routes>
+        <Route path='/' element={<Navigate to='/home'/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/tours/search' element={<SearchResultList/>}/>
+        <Route path='/tours/:id' element={<TourDetails/>}/>
+        <Route path='/tours' element={<Tours/>}/>
+    </Routes>
+  )
+}
+
+export default Routers
