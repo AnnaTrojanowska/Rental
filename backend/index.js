816d8af12bc7a5cb4cf6f98ef3853fef13f5c0e2
Vinan <80715419+AnnaTrojanowska@users.noreply.github.com>
2023-06-16 21:02:33 +0200

    some backend

--- backend/index.js
+++ backend/index.js
@@ -4,34 +4,37 @@ import mongoose from "mongoose"
 import cors from 'cors'
 import cookieParser from "cookie-parser"
 import tourRoute from './routes/tours.js'
+import userRoute from './routes/users.js'
 
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 8000
 
-mongoose.set("strictQuery", false )
+mongoose.set("strictQuery", false)
 
-const connect = async() => {
-    try{
+const connect = async () => {
+    try {
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser:true,
+            useNewUrlParser: true,
             useUnifiedTopology: true,
         })
         console.log('MongoDB databased connected')
-    } catch (err){
+    } catch (err) {
         console.log('MongoDB databased failed')
     }
 }
-
-// app.get("/", (req, res) => {
-//     res.send("api is working")
-// })
-
-
+app.post("/", (req, res) => {
+    res.send("api is working")
+})
 app.use(express.json())
+
 app.use(cors())
-app.use(cookieParser)
 app.use('/tours', tourRoute)
+app.use('/users', userRoute)
+app.use(cookieParser)
+
+
+
 
 
 app.listen(port, () => {
--- backend/routes/tours.js
+++ backend/routes/tours.js
@@ -1,8 +1,15 @@
 import express from 'express'
-import { createTour } from '../controllers/tourController.js'
+import { createTour, deleteTour, getAllTour, getFeaturedTour, getSingleTour, getTourBySearch, updateTour, getTourCount } from '../controllers/tourController.js'
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/', createTour)
+router.post('/', createTour);
+router.put('/:id', updateTour);
+router.delete('/:id', deleteTour);
+router.get('/:id', getSingleTour);
+router.get('/', getAllTour);
+router.get('/search/getTourBySearch', getTourBySearch)
+router.get('/search/getFeaturedTour', getFeaturedTour)
+router.get('/search/getTourCount', getTourCount)
 
-export default router 
+export default router;
